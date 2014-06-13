06100970f5f4cbacc99d4fa7122dbf1494ea4552
Damien Baty <damien.baty@polyconseil.fr>
2014-06-13 18:20:17 +0200

    harvesters: Remove 'all' symbol and leave the pub now.

--- dokang/harvesters/html.py
+++ dokang/harvesters/html.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2011-2014 Polyconseil SAS. All rights reserved.
 
-all = ['html_config', 'HtmlHarvester']
-
 from bs4 import BeautifulSoup
 
 from dokang.harvesters import Harvester
