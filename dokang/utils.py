5c07cddad53162f7ed363d250a794a8622c50f42
Damien Baty <damien.baty@polyconseil.fr>
2014-06-18 18:59:58 +0200

    Use alternative config loading strategy.
    In `doc_sets.py`, we had to use `inspect` to get the path of the file
    and build document set paths from it. By using `imp.load_source()`, we
    can use '__file__' instead, which looks less scary.

--- dokang/utils.py
+++ dokang/utils.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2011-2014 Polyconseil SAS. All rights reserved.
 
+import imp
+
+
 def load_doc_sets(settings):
     """Given a settings dictionnary with the path of the doc sets file,
     replace the path (in-place) with the doc sets themselves.
     """
     key = 'dokang.doc_sets'
     path = settings[key]
-    contents = {}
-    with open(path) as fp:
-        exec(fp.read(), {}, contents)  # pylint: disable=exec-used
+    module = imp.load_source('dokang__doc_sets', path)
     sets = {}
-    for info in contents['DOC_SETS']:
+    for info in module.DOC_SETS:
         sets[info['id']] = info
     settings[key] = sets
     return settings
