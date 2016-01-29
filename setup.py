894a191998e4c4d3e3cb0ca483dc0ac4dfe8962d
Damien Baty <damien.baty@polyconseil.fr>
2016-01-29 18:39:33 +0100

    Dokang is compatible with Python 3.5.

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
